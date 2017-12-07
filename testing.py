729a7de2215d88cf674c0d1684ddbefc20d0b20c
atefehrahimi <34093112+atefehrahimi@users.noreply.github.com>
2017-12-07 23:23:39 +0330

    Update testing.py
    testing_digit for number_digit

--- testing.py
+++ testing.py
@@ -1,22 +1,15 @@
-from unittest import TestCase
+import unittest
+from validation_digit import number_validation
 
-import lib
+
+class TestDigit(unittest.TestCase):
+    def test_digit_validation(self):
+        sample1 = "123445"                            # a sample1 for check function if return True
+        self.assertTrue(number_validation(sample1))
+
+        sample2 = "a string "                        # a sample2 for check function if raise a exception
+        self.assertRaises(ValueError, number_validation, sample2)
 
 
-class TestLib(TestCase):
-    def test_email_validation(self):
-        sample_1 = "invalid"
-        self.assertFalse(lib.email_validation(sample_1))
-
-        sample_2 = "me@morteza"
-        self.assertFalse(lib.email_validation(sample_2))
-
-        sample_3 = "me@morteza.123"
-        self.assertFalse(lib.email_validation(sample_3))
-
-        sample_4 = "me@morteza12@.com"
-        self.assertFalse(lib.email_validation(sample_4))
-
-        sample_5 = "me@motezana.com"
-        self.assertTrue(lib.email_validation(sample_5))
-
+if __name__ == '__main__':
+    unittest.main()
